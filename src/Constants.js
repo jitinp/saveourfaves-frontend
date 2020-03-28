5cc1db03bc07b16de009a77b4cdaa2b8e1c00312
jitinp <jitinp@nityaalabs.com>
2020-03-27 22:51:40 -0400

    updated API url

--- src/Constants.js
+++ src/Constants.js
@@ -1,7 +1,7 @@
 const Constants = {
   AddPlaceURL: "/addplace",
-  ApiURL: "http://ec2-99-79-50-232.ca-central-1.compute.amazonaws.com:8000/"
-  // ApiURL: "http://localhost:8000/",
+  // ApiURL: "http://localhost:8000"
+  ApiURL: "http://backend.saveyourfaves.ca/"
 };
 
 export default Constants;
